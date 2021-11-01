506b3b72fa3e01bb41f34dfca406a99e83c9aab2
Chutoro_Detteiu <57580049+Detteiu-Yoshida@users.noreply.github.com>
2021-11-01 23:24:47 +0900

    Update sort.py

--- sort.py
+++ sort.py
@@ -5,5 +5,4 @@ sort_list = str(start).split(',')
 loop = input('Please input number to sort')
 loop_int = int(loop)
 for i in range(0,loop_int):
-    a = random.randint(0,len(sort_list)-1)
-    print(sort_list[a])
+    print(sort_list.pop(random.randint(0,len(sort_list)-1)))
